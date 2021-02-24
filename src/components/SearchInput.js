190c4f8aebef579a33289d2128afbc9003a17a8f
eraygundogmus <gundogmuseray@gmail.com>
2021-02-24 15:54:13 +0300

    change api call functions to avoid too many request

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -9,7 +9,6 @@ const capitalize = (s) => {
 }
 
 
-
 class SearchInput extends React.Component {
     constructor(props) {
         super(props)
@@ -24,14 +23,21 @@ class SearchInput extends React.Component {
             temp: '273.1',
             feels: '273.1',
             tempmax: '273.1',
-            tempmin : '273.1'
+            tempmin: '273.1',
+            turkey: ['Adana', 'Adıyaman', 'Afyon', 'Ağrı', 'Amasya', 'Ankara', 'Antalya', 'Artvin',
+            'Aydın', 'Balıkesir', 'Bilecik', 'Bingöl', 'Bitlis', 'Bolu', 'Burdur', 'Bursa', 'Çanakkale',
+            'Çankırı', 'Çorum', 'Denizli', 'Diyarbakır', 'Edirne', 'Elazığ', 'Erzincan', 'Erzurum', 'Eskişehir',
+            'Gaziantep', 'Giresun', 'Gümüşhane', 'Hakkari', 'Hatay', 'Isparta', 'Mersin', 'İstanbul', 'İzmir',
+            'Kars', 'Kastamonu', 'Kayseri', 'Kırklareli', 'Kırşehir', 'Kocaeli', 'Konya', 'Kütahya', 'Malatya',
+            'Manisa', 'Kahramanmaraş', 'Mardin', 'Muğla', 'Muş', 'Nevşehir', 'Niğde', 'Ordu', 'Rize', 'Sakarya',
+            'Samsun', 'Siirt', 'Sinop', 'Sivas', 'Tekirdağ', 'Tokat', 'Trabzon', 'Tunceli', 'Şanlıurfa', 'Uşak',
+            'Van', 'Yozgat', 'Zonguldak', 'Aksaray', 'Bayburt', 'Karaman', 'Kırıkkale', 'Batman', 'Şırnak',
+            'Bartın', 'Ardahan', 'Iğdır', 'Yalova', 'Karabük', 'Kilis', 'Osmaniye', 'Düzce']
         };
         this.componentDidMount = this.componentDidMount.bind(this)
         this.changeHandler = this.changeHandler.bind(this)
         this.submitHandler = this.submitHandler.bind(this)
     }  
-
-
 
     changeHandler(event) {
         this.setState({ value: event.target.value });   
@@ -62,9 +68,6 @@ class SearchInput extends React.Component {
              const tempmin = searchdata.main.temp_min
              this.setState({ tempmin })
              
-             const search__result = () => {
-                 
-             }
          })
          .catch((err) => {
              console.log(err)
@@ -117,7 +120,7 @@ class SearchInput extends React.Component {
                         {/* <div> {this.state.initialLat} {this.state.initialLong}</div> */}
                         <button type="submit" value="search">Search </button>
                         <div className="search_result">
-                        <div className="location__cities">
+                        <div className="search__cities">
                                 <p> {capitalize(this.state.searchdata.name)}  </p>
                                 <p> Cloud : {this.state.clouds}% <br />
                                     Status : {capitalize(this.state.description)} <br />
@@ -144,7 +147,7 @@ class SearchInput extends React.Component {
                                 </div>
                                 ))}
                         </div>
-                
+               
             </div>
         )
     }
